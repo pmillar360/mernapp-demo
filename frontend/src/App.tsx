2e5b1015ad82738925484b427100a8f5744e7550
Patrick <patrickvmillar@gmail.com>
2024-07-25 12:32:19 -0400

    Implemented home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -26,7 +26,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={
           <Layout>
-            <p>Home Page</p>
+            <Home />
           </Layout>} />
         <Route path="/search" element={
           <Layout>
